d5d2791d30e4e6e109699f7748f8310326b33db2
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-15 17:10:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
   get '/recipes/new' do
     erb :new
   end
-  
+
   post '/recipes' do
     recipe = Recipe.create name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]
     redirect "/recipes/#{recipe.id}"
