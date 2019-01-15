232969caed5c0b63b95f7edb1ffdc69a933cde19
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-15 17:03:26 +0000

    Automatically backed up by Learn

--- db/migrate/20190115165136_recipes.rb.rb
+++ db/migrate/20190115165136_recipes.rb.rb
@@ -1,4 +1,9 @@
 class Recipes.rb < ActiveRecord::Migration
   def change
+    create_table :recipes do |r|
+      r.string :name
+      r.string :ingredients
+      r.string :cook_time
+    end
   end
 end
